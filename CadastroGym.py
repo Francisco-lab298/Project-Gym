5025b0f6d030afb0d25791dee4e7beb37a8c5788
Francisco-lab298 <francisschmidt2026@gmail.com>
2025-06-13 23:19:55 -0300

    Registro atualizo

---
+++ CadastroGym.py
@@ -0,0 +1,32 @@
+def registroGym():
+    nome = str(input("Digite seu nome: "))
+    QualSeuSexo = input("Qual o seu sexo? [masculino] [feminino]: ")
+    if QualSeuSexo == "masculino":
+        print("Homem")
+    elif QualSeuSexo == "feminino":
+        print("Mulher")
+    else:
+        while True:
+            print("Somente masculino ou feminino")
+            QualSeuSexo = input("Qual o seu sexo? [masculino] [feminino]: ")
+            if QualSeuSexo == "masculino":
+                print("Homem")
+                break
+            elif QualSeuSexo == "feminino":
+                print("Mulher")
+                break
+
+
+    idade = int(input("Sua Idade atual: "))#int para só deixar entra numero /  input so solicita info
+    QuerTomarSuplemento = input("Quer tomar suplemento [sim] [não]: ")#questionamento
+    if QuerTomarSuplemento == "sim":#se ele quiser em baixo vai ter mensagem
+        print("Suplementará")#mensagem aviso
+    elif QuerTomarSuplemento == "não":#se ele não quiser em baixo vai ter mensagem
+        print("Somente treino")#mensagem aviso
+    else:
+        print("Preencha somente (sim ou não)")
+    altura = float(input("Sua Altura: "))#Float representa numero em casas decimais
+    print(f"{altura:}")
+    peso = int(input("Qual o seu peso: "))
+    email = str(input("Digite seu e-mail: "))
+registroGym()
