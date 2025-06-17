170c5c856491d55c0300860c0a4bf41540622c8d
Francisco-lab298 <francisschmidt2026@gmail.com>
2025-06-16 22:52:21 -0300

    atualização

--- CadastroGym.py
+++ CadastroGym.py
@@ -1,5 +1,8 @@
+import string
+
+
 def registroGym():
-    nome = str(input("Digite seu nome: "))
+    nome = str(input("Digite seu nome: "))#teste
     QualSeuSexo = input("Qual o seu sexo? [masculino] [feminino]: ")
     if QualSeuSexo == "masculino":
         print("Homem")
@@ -16,15 +19,24 @@ def registroGym():
                 print("Mulher")
                 break
 
-
     idade = int(input("Sua Idade atual: "))#int para só deixar entra numero /  input so solicita info
+
     QuerTomarSuplemento = input("Quer tomar suplemento [sim] [não]: ")#questionamento
     if QuerTomarSuplemento == "sim":#se ele quiser em baixo vai ter mensagem
         print("Suplementará")#mensagem aviso
     elif QuerTomarSuplemento == "não":#se ele não quiser em baixo vai ter mensagem
         print("Somente treino")#mensagem aviso
     else:
-        print("Preencha somente (sim ou não)")
+        while True:
+            print("Preencha somente (sim ou não)")
+            QuerTomarSuplemento = input("Quer tomar suplemento [sim] [não]: ")
+            if QuerTomarSuplemento == "sim":
+                print("Homem")
+                break
+            elif QuerTomarSuplemento == "não":
+                print("Somente treino")
+                break
+
     altura = float(input("Sua Altura: "))#Float representa numero em casas decimais
     print(f"{altura:}")
     peso = int(input("Qual o seu peso: "))
